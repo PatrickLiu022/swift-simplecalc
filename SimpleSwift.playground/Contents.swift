2589ab77095da4a37e9171122dc0654abd682fed
patrickliu022 <patrickliu022@gmail.com>
2022-04-08 02:51:33 -0700

    did extra credit part 2

--- SimpleSwift.playground/Contents.swift
+++ SimpleSwift.playground/Contents.swift
@@ -1,23 +1,22 @@
 import CoreFoundation
 print("Welcome to the UW Calculator Playground")
 
-func calculate(_ args: [String]) -> Int {
-    
-    let avg : ([String]) -> Int = {
+func utils() -> (([String]) -> Double, ([String]) -> Double) {
+    let avg1 : ([String]) -> Double = {
         (list) in
-        var sum = 0
+        var sum = 0.0
         if (list.count == 1 && Int(list[0]) == nil) {
-            return 0
+            return 0.0
         }
         for i in list {
             if Int(i) != nil {
-                sum += Int(i)!
+                sum += Double(i)!
             }
         }
-        return sum / (list.count - 1)
+        return sum / (Double(list.count) - 1)
     }
     
-    let fact : ([String]) -> Int = {
+    let fact1 : ([String]) -> Double = {
         (list) in
         if (Int(list[0]) == nil) {
             return 0
@@ -32,57 +31,97 @@ func calculate(_ args: [String]) -> Int {
             return 0
         }
         
-        return fact
+        return Double(fact)
     }
+    
+    
+    return (avg1, fact1)
+}
+
+func switchCase(_ args: [String], _ index: Int, _ avg: ([String]) -> Double, _ fact: ([String]) -> Double) -> Double {
+    let operation = args[index]
+    var answer = 0.0
+    
+    switch operation {
+    case "+":
+        answer = Double(args[0])! + Double(args[2])!
+    case "-":
+        answer = Double(args[0])! - Double(args[2])!
+    case "*":
+        answer = Double(args[0])! * Double(args[2])!
+    case "/":
+        answer = Double(args[0])! / Double(args[2])!
+    case "%":
+        let leftSide = Double(args[0])!
+        let rightSide = Double(args[2])!
+        let intAnswer = Int(leftSide) % Int(rightSide)
+        answer = Double(intAnswer)
+    case "count":
+        answer = Double(args.count - 1)
+    case "avg":
+        answer = avg(args)
+    case "fact":
+        answer = fact(args)
+    default:
+        answer = -1.0
+        print("error")
+    }
+    return answer
+}
+
+func calculate(_ args: [String]) -> Int {
     
     var maybeAnInteger : Int? = 0
     var index = 0
-    var answer = 0
+    var answer = 0.0
+    
+    let utils = utils()
+    let avg = utils.0
+    let fact = utils.1
 
     while maybeAnInteger != nil {
         if index >= args.count {
             maybeAnInteger = nil
         } else {
             if Int(args[index]) == nil {
-                let operation = args[index]
-                switch operation {
-                case "+":
-                    answer = Int(args[0])! + Int(args[2])!
-                case "-":
-                    answer = Int(args[0])! - Int(args[2])!
-                case "*":
-                    answer = Int(args[0])! * Int(args[2])!
-                case "/":
-                    answer = Int(args[0])! / Int(args[2])!
-                case "%":
-                    answer = Int(args[0])! % Int(args[2])!
-                case "count":
-                    answer = args.count - 1
-                case "avg":
-                    answer = avg(args)
-                case "fact":
-                    answer = fact(args)
-                default:
-                    answer = -1
-                    print("error")
-                }
+               answer = switchCase(args, index, avg, fact)
             }
         }
         index += 1
     }
     
-    return answer
+    let flooredAnswer = floor(answer)
+    return Int(flooredAnswer)
 }
 
 func calculate(_ arg: String) -> Int {
-    var subSeqArr = arg.split(separator: " ")
+    let subSeqArr = arg.split(separator: " ")
     var list : [String] = []
     
     for i in subSeqArr {
         list.append(String(i))
     }
-    let result = calculate(list)
-    return result
+    var maybeAnInteger : Int? = 0
+    var index = 0
+    var answer = 0.0
+    
+    let utils = utils()
+    let avg = utils.0
+    let fact = utils.1
+
+    while maybeAnInteger != nil {
+        if index >= list.count {
+            maybeAnInteger = nil
+        } else {
+            if Int(list[index]) == nil {
+               answer = switchCase(list, index, avg, fact)
+            }
+        }
+        index += 1
+    }
+    
+    let flooredAnswer = floor(answer)
+    return Int(flooredAnswer)
 }
 
 // -------------------------------------------
@@ -146,15 +185,60 @@ calculate("-2 / 2") == -1
 
 calculate("1 -2 3 -4 5 count") == 5
 
- 
 // Implement calculate([String]) and calculate(String)
 // to use floating-point values
-/*
+
 func calculate(_ args: [String]) -> Double {
-    return -1.0
+    var maybeAnInteger : Int? = 0
+    var index = 0
+    var answer = 0.0
+    
+    let utils = utils()
+    let avg = utils.0
+    let fact = utils.1
+
+    while maybeAnInteger != nil {
+        if index >= args.count {
+            maybeAnInteger = nil
+        } else {
+            if Double(args[index]) == nil {
+                answer = switchCase(args, index, avg, fact)
+            }
+        }
+        index += 1
+    }
+    
+    return answer
 }
+
+
 func calculate(_ arg: String) -> Double {
-    return -1.0
+    let subSeqArr = arg.split(separator: " ")
+    var list : [String] = []
+    
+    for i in subSeqArr {
+        list.append(String(i))
+    }
+    var maybeAnInteger : Int? = 0
+    var index = 0
+    var answer = 0.0
+    
+    let utils = utils()
+    let avg = utils.0
+    let fact = utils.1
+
+    while maybeAnInteger != nil {
+        if index >= list.count {
+            maybeAnInteger = nil
+        } else {
+            if Double(list[index]) == nil {
+                answer = switchCase(list, index, avg, fact)
+            }
+        }
+        index += 1
+    }
+    
+    return answer
 }
 
 calculate(["2.0", "+", "2.0"]) == 4.0
@@ -163,5 +247,5 @@ calculate(["12.0", "-", "12.0"]) == 0.0
 calculate(["2.5", "*", "2.5"]) == 6.25
 calculate(["2.0", "/", "2.0"]) == 1.0
 calculate(["2.0", "%", "2.0"]) == 0.0
-calculate("1.0 2.0 3.0 4.0 5.0 count") == 5
- */
+calculate("1.0 2.0 3.0 4.0 5.0 count") == 5.0
+
